6e34d9d95105a1032d582ff709fa598534c122cf
Saori Matsumoto <matsumoto@route-zero.com>
2019-03-08 01:12:27 +0900

    Create Character.java

---
+++ RPG1/Character.java
@@ -0,0 +1,12 @@
+
+public class Character {
+	public int hp;
+	public int mp;
+	public String name;
+
+	public void introduce(){
+		System.out.println("私の名前は"+name+"だよ！！"+"体力は"+hp+"で、"+"魔力は"+mp+"だよ！よろしくね！！");
+
+	}
+
+}
