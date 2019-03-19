f835c67bc43830857511c23126d7b958e5279a82
Saori Matsumoto <matsumoto@route-zero.com>
2019-03-19 21:48:02 +0900

    Update RPGcharacter.java

--- RPGcharacter.java
+++ RPGcharacter.java
@@ -1,25 +1,6 @@
 
 public class RPGcharacter {
 	public static void main(String[] args){
-		Character a;
-		a = new Character();
-
-		a.hp = 80;
-		a.mp = 70;
-		a.clash = 20;
-		a.name ="さくら";
-
-
-		Character z;
-		z = new Character();
-
-		z.hp = 65;
-		z.mp = 90;
-		z.clash = 30;
-		z.name ="みかん";
-
-		a.introduce();
-		z.introduce();
 
 		Character a1 = new Character("さくら",20,80);
 
