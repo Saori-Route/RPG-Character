1d60eeeff50a7252cc5fcaac098bf5994a33bbac
Saori Matsumoto <matsumoto@route-zero.com>
2019-03-19 21:06:17 +0900

    Update RPGcharacter.java

--- RPGcharacter.java
+++ RPGcharacter.java
@@ -6,7 +6,7 @@ public class RPGcharacter {
 
 		a.hp = 80;
 		a.mp = 70;
-		a.clash = 30;
+		a.clash = 20;
 		a.name ="さくら";
 
 
@@ -15,23 +15,23 @@ public class RPGcharacter {
 
 		z.hp = 65;
 		z.mp = 90;
-		z.clash = 35;
+		z.clash = 30;
 		z.name ="みかん";
 
 		a.introduce();
 		z.introduce();
 
-		Character a1 = new Character("さくら",35,80);
+		Character a1 = new Character("さくら",20,80);
 
 		Character z1 = new Character("みかん",30,65);
 
 
 		a1.attack(z1);
-			System.out.println("きゃああああああああああああああああああああ");
+		System.out.println("きゃああああああああああああああああああああ");
 
-		z1.attack(a1);{
-			System.out.println("まだまだああああああああああああああああああ");
-		}
+		z1.attack(a1);
+		System.out.println("まだまだああああああああああああああああああ");
+
 
 	}
 
