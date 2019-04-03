0fa1883dded721e1c59da3c0e6b5ce1226de9d82
Saori Matsumoto <matsumoto@route-zero.com>
2019-04-03 23:47:35 +0900

    Create Knight.java

---
+++ Knight.java
@@ -0,0 +1,31 @@
+
+class Knight extends Character {
+	private String sword;
+
+	public Knight() {
+
+	}
+
+
+	public void introduce(){
+		System.out.println("俺の名前は"+name+"！！"+"ナイトだ！");
+
+	}
+
+
+	public Knight(String name,String swrod) {
+		super(name,80,70);
+		this.sword= swrod;
+
+	}
+
+	public void attack(Character target) {
+		sword();
+		target.damage(clash);
+	}
+
+	public void sword() {
+		System.out.println(this.name+"の攻撃"+this.sword+"！！");
+	}
+
+}
