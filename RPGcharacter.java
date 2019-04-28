d85c4611311184aa7df921002eb253cf87b04f9a
Saori Matsumoto <matsumoto@route-zero.com>
2019-04-29 03:00:48 +0900

    Update RPGcharacter.java

--- RPGcharacter.java
+++ RPGcharacter.java
@@ -2,23 +2,35 @@
 public class RPGcharacter {
 	public static void main(String[] args){
 
-		Knight a = new Knight("しょうた","80");
+
+        int s1=2;
+		int s2=2;
 
 
-		Magician c = new Magician("さくら","50");
+		Character s[]= new Character[s1];
+
+		s[0]= new Knight("しょうた","ファイヤーエンブレム");
+		s[1]= new Magician("さくら","滅びろ");
+
+		Character ss[]= new Character[s2];
+
+		ss[0]= new Healer("みかん","強烈な睡魔の呪文");
+		ss[1]= new Sages("サラマ","神の御言葉");
+
+		for(int i=0; i<s1; i++) {
+			((Ksb)s[i]).ksb(ss);
+
+		}
+
+		for(int j=0; j<s2; j++) {
 
 
-		int k =1;
+		    ((Wa)ss[j]).warau(s);
+		    ((Letter)ss[j]).letter(s);
+            ((Movable)ss[j]).move(s);
 
+		}
 
-        for(int i= 0; i<k; i++){
-             a.attack(c);{
-        	 c.attack(a);
-
-
-        	}
-
-        }
 
 
 	}
